7c9af19a5e7871b45b5b659736e2750074dbb0d2
Reinigen <raymundcrystal@yahoo.com>
2021-05-02 13:11:04 +0800

    Adding Enemy

--- Assets/PanelOpener.cs
+++ Assets/PanelOpener.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel; //Panel that needs to be opened
-    public Button AttackB; //Attack Button
-    public Button CastB; //Cast Button
-    public Button FuriteB; //Furite Button
-    public Button RunB; //Run button
+    public Button Ducky; //Ducky Enemy
+    public Button Goblin; //Cast Button
+    public Button Abberation; //Furite Button
+    public Button Slime; //Run button
+    
 
     public void PanelToggle()
     {
         if (!Panel.activeSelf) //Check if panel is not active
         {
             Panel.SetActive(true); //Set panel to active
-            AttackB.interactable = false; //Disable Attack Button
-            CastB.interactable = false; //Disable Cast Button
-            FuriteB.interactable = false; //Disable Furite Button
-            RunB.interactable = false; //Disable Run Button
+            Ducky.interactable = false; //Disable Attack Button
+            Goblin.interactable = false; //Disable Cast Button
+            Abberation.interactable = false; //Disable Furite Button
+            Slime.interactable = false; //Disable Run Button
         }
     }
 
@@ -29,9 +30,9 @@ public class PanelOpener : MonoBehaviour
         {
             Panel.SetActive(false); //Set panel to not active
             AttackB.interactable = true; //Enable Attack Button
-            CastB.interactable = true; //Enable Cast Button
-            FuriteB.interactable = true; //Enable Furite Button
-            RunB.interactable = true; //Enable Run Button
+            Goblin.interactable = true; //Enable Cast Button
+            Abberation.interactable = true; //Enable Furite Button
+            Slime.interactable = true; //Enable Run Button
         }
     }
 }
