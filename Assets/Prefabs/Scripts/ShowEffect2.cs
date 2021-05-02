55b16984320b06684bfcba8f6c5f11f835e0057b
gab-y01 <angelli.yap@obf.ateneo.edu>
2021-05-02 23:39:57 +0800

    Moved the panels so you can see them on a smaller screen

--- Assets/Prefabs/Scripts/ShowEffect2.cs
+++ Assets/Prefabs/Scripts/ShowEffect2.cs
@@ -5,11 +5,24 @@ using UnityEngine.UI;
 
 public class ShowEffect2 : MonoBehaviour
 {
-    public GameObject EffectTxtPrefab;
-    // Start is called before the first frame update
-    public void CastSpell()
-    {
-        Instantiate(EffectTxtPrefab);
-    }
+    //public GameObject Text;
+    public string Name;
 
+    //public void Start() {
+        //string spelln = GameObject.Find("Canvas/EffectTxt").GetComponent<string>();
+        //spelln.string = "You target " + Name + " with your spell";
+        //if (!spelln.activeSelf)
+        //{
+        //    spelln.SetActive(true);
+        //}
+    //}
+
+    //private void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.Escape))
+    //    {
+    //        string spelln = GameObject.Find("Canvas/EffectTxt").GetComponent<string>();
+    //        spelln.SetActive(false);
+    //    }
+    //}
 }
