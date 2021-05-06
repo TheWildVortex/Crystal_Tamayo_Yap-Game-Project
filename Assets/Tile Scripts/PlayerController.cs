57fb30f4913b699f9a00a409609b63dc600129d0
Brent Tamayo <thewildvortex900@gmail.com>
2021-05-06 18:20:51 +0800

    Added Steps for Player Movement
    Followed steps from the video regarding player movement. However, Sprite does not move

---
+++ Assets/Tile Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap groundTiles;
+    [SerializeField]
+    private Tilemap collisionTiles;
+
+    private @PlayerMovement controls;
+
+    private void Awake()
+    {
+        controls = new @PlayerMovement();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (CheckMove(direction))
+            transform.position += (Vector3)direction;
+    }
+
+    private bool CheckMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTiles.WorldToCell(transform.position + (Vector3)direction);
+
+        if (groundTiles.HasTile(gridPosition) || collisionTiles.HasTile(gridPosition))
+            return false;
+        return true;
+    }
+}
